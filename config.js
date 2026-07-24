1d09420c79d5acb91f511367e97bb351ae78abd7
RyanFol123 <ryanwasatlalaland@gmail.com>
2026-07-24 06:45:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // Replace this with the workers.dev URL printed after `npm run deploy`.
-window.DISCORD_STATS_API = "https://statsdiscordmc-api.statsdiscordmc.workers.dev/";
+window.DISCORD_STATS_API = "https://statsdiscordmc-api.discordstatsmc.workers.dev/";
